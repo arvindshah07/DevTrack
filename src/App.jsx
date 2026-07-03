4db68fab9ec7fb57f9dcaa4d5e82d54f8088b915
arvindshah07 <arvindshahofficial1729@gmail.com>
2026-07-03 22:56:50 +0530

    react and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <button className="bg-amber-400 text-black px-6 py-3 rounded-xl font-semibold hover:bg-amber-500 transition duration-300 cursor-pointer">
+      Test
+    </button>
+  );
+}
+
+export default App;
