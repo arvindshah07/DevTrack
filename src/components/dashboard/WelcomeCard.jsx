5623423b5204b7a9c9ffc79b59aae53be95f0d97
arvindshah07 <arvindshahofficial1729@gmail.com>
2026-07-25 21:46:03 +0530

    welcome card msg update

--- src/components/dashboard/WelcomeCard.jsx
+++ src/components/dashboard/WelcomeCard.jsx
@@ -6,7 +6,7 @@ function WelcomeCard() {
     <>
      <div className='bg-white rounded-xl shadow-md p-6'>
       <h1 className='text-3xl font-bold'>
-        Welcome, {username} Back 👋 
+        Welcome, {username} 👋 
         </h1>
       <p className='text-gray-600 mt-2'>
         Start managing your projects efficiently .
